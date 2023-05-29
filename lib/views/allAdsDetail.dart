ec7bfeb264935f45e7c958629c024c77fb67a09f
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-05-29 23:58:41 +0300

    Added checks whether there is a chat room or not in transition processes

--- lib/views/allAdsDetail.dart
+++ lib/views/allAdsDetail.dart
@@ -1,9 +1,8 @@
 // ignore_for_file: unnecessary_string_interpolations
-
-
-
 import 'package:campusgoo/utility/color.dart';
+import 'package:campusgoo/views/messageDetail.dart';
 import 'package:campusgoo/views/urunDetayMesaj.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -78,7 +77,7 @@ class _AllAdsDetailPageState extends State<AllAdsDetailPage> {
                 ? _myAds()
                 : IconButton(
                     onPressed: () {},
-                    icon: Icon(
+                    icon: const Icon(
                       Icons.share,
                       color: Colors.black,
                     ))
@@ -89,8 +88,9 @@ class _AllAdsDetailPageState extends State<AllAdsDetailPage> {
           children: [
             _imageContainer(context),
             _myDivider(),
-            
-            _myListtile("${widget.price} "+ " \u20ba", "${widget.name}",TextStyle(fontWeight: FontWeight.bold,fontSize: 24)),
+
+            _myListtile("${widget.price} " + " \u20ba", "${widget.name}",
+                const TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
             _myDivider(),
             const Text("    Detaylar",
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
@@ -104,11 +104,13 @@ class _AllAdsDetailPageState extends State<AllAdsDetailPage> {
               ),
             ),
             _myDivider(),
-            _myListtile(" Açıklama",  "  ${widget.aciklama}",TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
-  
+            _myListtile(" Açıklama", "  ${widget.aciklama}",
+                const TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+
             _myDivider(),
-            _myListtile(" Konum", "  ${widget.konum}",TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
-        
+            _myListtile(" Konum", "  ${widget.konum}",
+                const TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+
             _myDivider(),
             //_myListtile(metin, metin2)
             ListTile(
@@ -116,7 +118,7 @@ class _AllAdsDetailPageState extends State<AllAdsDetailPage> {
                   backgroundColor: mainColor.color,
                   child: IconButton(
                       onPressed: () {},
-                      icon: Icon(
+                      icon: const Icon(
                         Icons.person,
                         color: Colors.white,
                       ))),
@@ -130,7 +132,7 @@ class _AllAdsDetailPageState extends State<AllAdsDetailPage> {
               ),
             ),
             _myDivider(),
-            Container(
+            SizedBox(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
@@ -147,10 +149,9 @@ class _AllAdsDetailPageState extends State<AllAdsDetailPage> {
         ));
   }
 
-  ListTile _myListtile(String metin, String metin2,TextStyle style) {
+  ListTile _myListtile(String metin, String metin2, TextStyle style) {
     return ListTile(
-      title: Text(metin ,
-          style: style),
+      title: Text(metin, style: style),
       subtitle: Text(metin2, style: const TextStyle(fontSize: 16)),
     );
   }
@@ -170,9 +171,9 @@ class _AllAdsDetailPageState extends State<AllAdsDetailPage> {
     return IconButton(
         onPressed: () {
           Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (context) => Arayuz()));
+              context, MaterialPageRoute(builder: (context) => const Arayuz()));
         },
-        icon: Icon(
+        icon: const Icon(
           Icons.arrow_back,
           color: Colors.black,
         ));
@@ -183,7 +184,7 @@ class _AllAdsDetailPageState extends State<AllAdsDetailPage> {
       children: [
         IconButton(
             onPressed: () {},
-            icon: Icon(
+            icon: const Icon(
               Icons.share,
               color: Colors.black,
             )),
@@ -191,7 +192,7 @@ class _AllAdsDetailPageState extends State<AllAdsDetailPage> {
             onPressed: () {
               edit();
             },
-            icon: Icon(
+            icon: const Icon(
               Icons.edit,
               color: Colors.black,
             ))
@@ -204,17 +205,17 @@ class _AllAdsDetailPageState extends State<AllAdsDetailPage> {
       width: 150,
       child: ElevatedButton.icon(
           onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: ((context) => chat())));
+            Navigator.push(context,
+                MaterialPageRoute(builder: ((context) => const chat())));
           },
-          icon: Icon(Icons.handshake_outlined),
-          label: Text("Yüzyüze Talep"),
+          icon: const Icon(Icons.handshake_outlined),
+          label: const Text("Yüzyüze Talep"),
           style: ButtonStyle(
               backgroundColor: MaterialStateProperty.all(mainColor.color),
               shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.black))))),
+                      side: const BorderSide(color: Colors.black))))),
     );
   }
 
@@ -223,33 +224,94 @@ class _AllAdsDetailPageState extends State<AllAdsDetailPage> {
       width: 150,
       child: ElevatedButton.icon(
         onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: ((context) => urunDetayMesajlasma(
-                        userId: FirebaseAuth.instance.currentUser!.uid.toString(),
-                        conversationId: widget.postId.toString(),
-                        
-                        postId: widget.postId.toString(),
-                        postUserId: widget.postUserId.toString(),
-                        name: widget.name.toString(),
-                        price: widget.price!.toInt(),
-                        resim: widget.resim.toString(),
-                        konum: widget.konum.toString(),
-                        durum: widget.durum.toString(),
-                        user: widget.user.toString(),
-                        aciklama: widget.aciklama.toString(),
-                        
-                      ))));
+          print('here');
+          var uid = FirebaseAuth.instance.currentUser!.uid;
+          FirebaseFirestore.instance
+              .collection("chat_rooms")
+              .where('product_id', isEqualTo: widget.postId)
+              .where('users', arrayContains: uid)
+              .get()
+              .then((value) {
+            if (value.docs.isNotEmpty) {
+              print('is not new');
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MessageDetail(
+                    userId: FirebaseAuth.instance.currentUser!.uid.toString(),
+                    postId: widget.postId,
+                    //name: senderName.toString(),
+                    resim: widget.resim,
+                    product_name: widget.name,
+                    user: uid,
+                    roomId: value.docs[0].id,
+                    postUserId: widget.postUserId,
+                  ),
+                ),
+              );
+            } else {
+              print('is new');
+              FirebaseFirestore.instance.collection('chat_rooms').add({
+                // 'timeStamp': DateTime.now(),
+                // 'product_id': widget.postId,
+                // 'product_name': widget.name,
+                // 'images': widget.resim,
+                // 'users': [widget.postUserId, uid],
+                'lastMessage': '',
+                'timeStamp': DateTime.now(),
+                'senderId': uid,
+                'receiverId': widget.postUserId,
+                'product_id': widget.postId,
+                'product_name': widget.name,
+                'images': widget.resim,
+                'users': [widget.postUserId, uid],
+              }).then((value) {
+                print('id skfksldf' + value.id);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MessageDetail(
+                      userId: FirebaseAuth.instance.currentUser!.uid.toString(),
+                      postId: widget.postId,
+                      //name: senderName.toString(),
+                      resim: widget.resim,
+                      product_name: widget.name,
+                      user: uid,
+                      roomId: value.id,
+                    ),
+                  ),
+                );
+              });
+            }
+          });
+
+          // Navigator.push(
+          //     context,
+          //     MaterialPageRoute(
+          //         builder: ((context) => urunDetayMesajlasma(
+          //               userId: FirebaseAuth.instance.currentUser!.uid.toString(),
+          //               conversationId: widget.postId.toString(),
+          //
+          //               postId: widget.postId.toString(),
+          //               postUserId: widget.postUserId.toString(),
+          //               name: widget.name.toString(),
+          //               price: widget.price!.toInt(),
+          //               resim: widget.resim.toString(),
+          //               konum: widget.konum.toString(),
+          //               durum: widget.durum.toString(),
+          //               user: widget.user.toString(),
+          //               aciklama: widget.aciklama.toString(),
+          //
+          //             ))));
         },
-        icon: Icon(Icons.message),
-        label: Text("Sohbet"),
+        icon: const Icon(Icons.message),
+        label: const Text("Sohbet"),
         style: ButtonStyle(
             backgroundColor: MaterialStateProperty.all(mainColor.color),
             shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                 RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(color: Colors.black)))),
+                    side: const BorderSide(color: Colors.black)))),
       ),
     );
   }
