f081d97f48e9c2c6f5d869567614ca778b1d018d
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-05-29 23:59:44 +0300

    messageadded function changed

--- lib/views/messageDetail.dart
+++ lib/views/messageDetail.dart
@@ -10,22 +10,21 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class MessageDetail extends StatefulWidget {
-  MessageDetail({
-    super.key,
-    this.postId,
-    this.postUserId,
-    this.user,
-    this.resim,
-    this.name,
-    this.product_name,
-    this.userId,
-    required this.roomId,
-  });
+  MessageDetail(
+      {super.key,
+      this.postId,
+      this.postUserId,
+      this.user,
+      this.resim,
+      // this.name,
+      this.product_name,
+      this.userId,
+      required this.roomId});
   final String? postId;
   final String? postUserId;
   final String? user;
   final String? resim;
-  final String? name;
+  //final String? name;
   final String? product_name;
   final String? userId;
   final String roomId;
@@ -72,17 +71,21 @@ class _MessageDetailState extends State<MessageDetail> {
       if (snapshot.exists) {
         //kullanıcının adının firebase veritabanında bulunup bulunmadığını kontrol etmek için kullanılır
         String username = snapshot.data()!['name'];
-        FirebaseFirestore.instance.collection('chat_rooms').add({
-          'message': text,
-          'timeStamp': DateTime.now(),
-          'senderId': user,
-          'senderName': username,
-          'receiverId': widget.user,
-          'product_id': widget.postId,
-          'product_name': widget.name,
-          'images': widget.resim,
-          'users': [currentUser, widget.user],
+
+        FirebaseFirestore.instance
+            .collection('chat_rooms')
+            .doc(widget.roomId)
+            .collection('messages')
+            .add({
+          'date': DateTime.now(),
+          'from': user,
+          'to': widget.postUserId,
+          'message': messageController.text,
         }).then((value) {
+          FirebaseFirestore.instance
+              .collection("chat_rooms")
+              .doc(widget.roomId)
+              .update({'lastMessage': messageController.text});
           //mesaj firebase başarıyla kaydedildiğinde bu satır çalışır
           messageController.text = '';
           bildirimGoster(text);
@@ -97,36 +100,6 @@ class _MessageDetailState extends State<MessageDetail> {
     }).catchError((error) {
       print('Kullanıcı adını alma hatası: $error');
     });
-  }
-
-  Future<void> sendTextMessage(String roomId, String message) async {
-    var data = <String, dynamic>{
-      'from': FirebaseAuth.instance.currentUser!.uid,
-      'to': widget.postUserId,
-      'message': message,
-      'message_type': '0',
-      'isSeenFrom': true,
-      'isSeenTo': false,
-      'date': DateTime.now(),
-    };
-    FirebaseFirestore.instance
-        .collection('chat_rooms')
-        .doc(widget.roomId)
-        .collection('messages')
-        .add(data);
-  }
-
-  //String roomId = widget.roomId;
-  //final Stream<QuerySnapshot> chatStream = FirebaseFirestore.instance.collection('chat_rooms').doc(this.roomId).collection('messages').orderBy('date', descending: true).snapshots();
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    //room_id= widget.roomId;
-    //uygulama ilk açıldığında
-    //kurulum fonksiyonunu gerçekleştiriyoruz.
-    // Bununla işlerimizi halledebiliyoruz.
-    // androidKurulum();
   }
 
   void messageRemoved() {
@@ -165,6 +138,16 @@ class _MessageDetailState extends State<MessageDetail> {
     }
   }
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //uygulama ilk açıldığında
+    //kurulum fonksiyonunu gerçekleştiriyoruz.
+    // Bununla işlerimizi halledebiliyoruz.
+    // androidKurulum();
+  }
+
   Future<void> bildirimGoster(String message) async {
     var androidBildirimDetay = const AndroidNotificationDetails(
         "kanal id", "kanal başlık",
@@ -184,6 +167,12 @@ class _MessageDetailState extends State<MessageDetail> {
 
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> chatStream = FirebaseFirestore.instance
+        .collection('chat_rooms')
+        .doc(widget.roomId)
+        .collection('messages')
+        .orderBy('date', descending: false)
+        .snapshots();
     //final String userId = "pceXDyA3HagfmzQ8vyXw8vokOaz1";
     // final Stream<QuerySnapshot> messageStream = FirebaseFirestore.instance
     //     .collection('chats')
@@ -192,29 +181,21 @@ class _MessageDetailState extends State<MessageDetail> {
     //     .orderBy("timeStamp", descending: false)
     //     .snapshots();
 
-    // final Stream<QuerySnapshot> messageStream = FirebaseFirestore.instance
-    //     .collection('chat_rooms')
-    //     .doc(widget.roomId)
-    //     .collection('messages')
-    //     .snapshots();
-    //     print("hellüü");
-    //     print(widget.roomId);
-    //     print("helllü");
-
-    final Stream<QuerySnapshot> messageStream=FirebaseFirestore.instance
-    .collection('chat_rooms').doc(widget.roomId).collection('messages').orderBy("date",descending: false).snapshots();
-    
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
             onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Arayuz()));
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Arayuz()));
             },
-            icon: Icon(
+            icon: const Icon(
               Icons.arrow_back,
               color: Colors.black,
             )),
+        // title: Text(
+        //   widget.product_name.toString(),
+        //   style: const TextStyle(color: Colors.black, fontSize: 15),
+        // ),
         title: Row(
           children: [
             SizedBox(
@@ -223,16 +204,16 @@ class _MessageDetailState extends State<MessageDetail> {
                 child: Image.network(widget.resim.toString())),
             Expanded(
                 child: ListTile(
-              title: Text(
-                widget.name.toString(),
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold),
-              ),
+              // title: Text(
+              //   widget.name.toString(),
+              //   style: const TextStyle(
+              //       color: Colors.black,
+              //       fontSize: 12,
+              //       fontWeight: FontWeight.bold),
+              // ),
               subtitle: Text(
                 widget.product_name.toString(),
-                style: TextStyle(color: Colors.black, fontSize: 15),
+                style: const TextStyle(color: Colors.black, fontSize: 15),
               ),
             ))
           ],
@@ -242,7 +223,7 @@ class _MessageDetailState extends State<MessageDetail> {
             children: [
               IconButton(
                   onPressed: () {},
-                  icon: Icon(
+                  icon: const Icon(
                     Icons.call,
                     color: Colors.black,
                   )),
@@ -252,19 +233,19 @@ class _MessageDetailState extends State<MessageDetail> {
         ],
       ),
       body: StreamBuilder(
-          stream: messageStream,
+          stream: chatStream,
           builder:
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (snapshot.hasError) {
               return Text('Error : ${snapshot.error}');
             }
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return Text("Loading...");
+              return const Text("Loading...");
             }
 
             return Column(children: [
               _messageGet(snapshot),
-              Divider(
+              const Divider(
                 thickness: 3,
               ),
               Row(
@@ -293,24 +274,24 @@ class _MessageDetailState extends State<MessageDetail> {
   }
 
   ListTile _myMessageScreen(QueryDocumentSnapshot<Object?> doc) {
-    final bool isCurrentUser = widget.userId == doc['senderId'];
+    final bool isCurrentUser = widget.userId == doc['from'];
 
     return ListTile(
       title: Align(
           alignment:
               isCurrentUser ? Alignment.centerRight : Alignment.centerLeft,
           child: Container(
-              padding: EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
               decoration: BoxDecoration(
                   color: isCurrentUser
                       ? mainColor.color
                       : Colors.green, // Mesaj gönderenin ve alıcının renkleri
-                  borderRadius: BorderRadius.horizontal(
+                  borderRadius: const BorderRadius.horizontal(
                     left: Radius.circular(10),
                     right: Radius.circular(10),
                   )),
-              child:
-                  Text(doc['message'], style: TextStyle(color: Colors.white)))),
+              child: Text(doc['message'],
+                  style: const TextStyle(color: Colors.white)))),
     );
   }
 
@@ -325,7 +306,7 @@ class _MessageDetailState extends State<MessageDetail> {
 
     return Expanded(
         child: Container(
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
                 borderRadius: BorderRadius.horizontal(
                     left: Radius.circular(25), right: Radius.circular(25))),
             child: Row(
@@ -333,7 +314,7 @@ class _MessageDetailState extends State<MessageDetail> {
                 Expanded(
                   child: TextField(
                     onSubmitted: submitMessage,
-                    decoration: InputDecoration(hintText: "Mesaj"),
+                    decoration: const InputDecoration(hintText: "Mesaj"),
                     controller: messageController,
                   ),
                 ),
@@ -343,21 +324,21 @@ class _MessageDetailState extends State<MessageDetail> {
                       messageAdded(message);
                       bildirimGoster(message);
                     },
-                    icon: Icon(Icons.send))
+                    icon: const Icon(Icons.send))
               ],
             )));
   }
 
   PopupMenuButton<int> _messageRemovedPopupMenuButton() {
     return PopupMenuButton(
-      icon: Icon(
+      icon: const Icon(
         Icons.more_vert,
         color: Colors.black,
       ),
       itemBuilder: ((context) => [
-            PopupMenuItem(
-              child: Text("Sohbeti sil"),
+            const PopupMenuItem(
               value: 1,
+              child: Text("Sohbeti sil"),
             ),
           ]),
       onSelected: ((value) {
